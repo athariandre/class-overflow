bf23f2de6e6996b42987a8c844ce0923abd381e7
athariandre <athariandre@gmail.com>
2024-09-29 10:16:27 -0500

    hi

--- backend/app.py
+++ backend/app.py
@@ -29,6 +29,8 @@ def get_course_info():
         professor_name = course['professor_name']
         try:
             [profGPA, exGPA] = getAverageGPA(department, int(course_code), professor_name)
+            if(profGPA == -1 or exGPA == -1):
+                raise Exception("Prof/CourseNum/Dep Does not exist!")
             [RMP_rating, RMP_would_take_again, RMP_difficulty] = get_professor_stats(professor_name)
         except Exception as e:
             out = {
@@ -41,26 +43,20 @@ def get_course_info():
 
 
 
-        RMP_would_take_again = int(RMP_would_take_again[:2])
-        RMP_difficulty = float(RMP_difficulty[:2])
-        RMP_rating = float(RMP_rating)
-        profGPA = float(profGPA)
-        exGPA = float(exGPA)
+        RMP_would_take_again = float(RMP_would_take_again[:2]) / 100.0
+        RMP_difficulty = float(RMP_difficulty[:2]) # 0-5
+        RMP_rating = float(RMP_rating) #0-5
+        profGPA = float(profGPA) #0-4
+        exGPA = float(exGPA) #0-4
 
         w1 = 0.4
-        w2 = 0.4
-        w3 = 0.2
+        w2 = 0.15
+        w3 = 0.15
+        w4 = 0.3
 
-        classDifficulty = w1 * (1 - (exGPA / 4.0)) + ((w2 * RMP_difficulty) / 5.0) + w3 * (1 - (RMP_would_take_again / 100.0))
-
-        w1 = 0.5
-        w2 = 0.3
-        w3 = 0.2
-
-        profDifficultyImpact = w1 * (1 - (profGPA / 4.0)) + w2 * (classDifficulty / 5.0) + w3 * (1 - (RMP_would_take_again / 100.0))
-
-        classDiffScore = classDifficulty * 70 + 53
-        profDiffScore = profDifficultyImpact * 70 + 57
+        class_diff_score = (w1*(1 - (exGPA / 4)) + w2*(1 - (RMP_rating / 5.0)) + w3*(1-(RMP_would_take_again / 100.0)) + w4*(RMP_difficulty / 5.0)) * 100 + 32
+        prof_diff_index = 1 - ((profGPA - exGPA) / (exGPA)) # ex. profGPA 4 exGPA 3 diffIdx = 0.75 so 75% as hard
+        screwage = (class_diff_score * prof_diff_index)
 
         class_reddit_scrape(department, course_code)
         # Load the object from the pickle file
@@ -82,22 +78,21 @@ def get_course_info():
         completion = client.chat.completions.create(
             model="gpt-4o-mini",
             messages=[
-                {"role": "system", "content": "I want you to infer the difficulty of the class using quotes from any of the posts. When quoting the posts, make sure to quote things directly and do not change the content of the quotes. Also, do NOT mention any quotes that specifically talk about a teacher, keep it strictly talking about the course itself. "},
+                {"role": "system", "content": "I want you to infer the difficulty of the class. When respondin do NOT mention any quotes that specifically talk about a teacher, keep it strictly talking about the course itself. "},
                 {
                     "role": "user",
-                    "content": f"I want you to infer the difficulty of the class using quotes from any of the posts. When quoting the posts, make sure to quote things directly and do not change the content of the quotes. Also, do NOT mention any quotes that specifically talk about a teacher, keep it strictly talking about the course itself: {department} {course_code} / {(string_data + csv_string)}"
+                    "content": f"I want you to infer the difficulty of the class in 1-2 concise sentences ONLY. When respondin do NOT mention any quotes that specifically talk about a teacher, keep it strictly talking about the course itself. {department} {course_code} / {(string_data + csv_string)}"
                 }
             ]
         )
         reddit_summary = completion.choices[0].message.content
-        screwage = classDiffScore + profDiffScore
 
         results.append({
             'department': department,
             'course_code': course_code,
             'professor_name': professor_name,
-            'class_difficulty': classDiffScore,
-            'professor_difficulty': profDiffScore,
+            'class_difficulty': class_diff_score,
+            'professor_difficulty': prof_diff_index,
             'reddit_summary': reddit_summary,
             'screw_percentage': screwage
         })
